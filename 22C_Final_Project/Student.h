5daa6aa51b6f4d2108dbb1356307d7c048b0bfee
aly-aly <wang.jaye@yahoo.com>
2017-12-07 12:14:14 -0800

    input.txt final version

--- 22C_Final_Project/Student.h
+++ 22C_Final_Project/Student.h
@@ -111,32 +111,32 @@ bool Student::operator!=(const Student& theStudent) const
 
 
 /*
-Name                    ID         GPA       IsFullTime      EnrollDate    UnitsCompleted
-Lillian   Porter        24622366   1.5       full-time       10/12/14      12
-Justin   Moore          48544988   3.78      full-time       03/04/16      23
-Tim   Nichols           77370250   2.45      part-time       03/04/12      87
-Stanley   Griffin       44918524   3.56      part-time       03/04/15      33
-Brendan   Marsh         52894630   3.89      full-time       03/04/12      99
-Nancy   Cox             70892940   2.96      full-time       03/04/15      100
-Carmen   Holmes         98833494   1.37      full-time       03/04/15      22
-Dora   Blake            55898602   1.45      part-time       02/04/11      99
-Raymond   Lamb          89619733   2.68      part-time       03/04/15      22
-Mary   Lee              94500541   3.67      full-time       01/04/13      98
-Kirk   Holt             32667798   4.00      part-time       03/04/15      08
-Wesley   Valdez         84301406   0.34      part-time       07/04/12      89
-Jimmie   Cook           94234524   0.98      full-time       08/04/11      87
-Kristen   Mccarthy      87789460   2.57      part-time       12/04/13      37
-Isabel   Zimmerman      55418766   1.23      full-time       03/04/15      72
-Denise   Goodman        83615900   3.45      part-time       03/03/14      22
-Oscar   Neal            83615900   1.47      part-time       03/02/11      22
-Dana   Miles            52972391   0.38      full-time       03/04/15      122
-Bernadette   Taylor     57086018   0.98      part-time       03/11/15      32
-Meredith   Lambert      29099997   2.97      full-time       03/08/11      23
-Marlene   Phillips      59374222   4.00      part-time       03/09/16      42
-Edward   Massey         54688476   3.66      part-time       03/01/15      32
-Kyle   Aguilar          12466249   3.29      full-time       03/04/12      92
-Suzanne   Briggs        18528076   3.40      full-time       03/08/13      97
-Yvette   Morgan         69459190   4.00      part-time       01/03/12      24
+ Name                    ID         GPA       IsFullTime      EnrollDate    UnitsCompleted
+ Stanley Griffin     44918524     3.56         part-time    03/04/15     33
+ Kyle Aguilar        12466249     3.29         full-time    03/04/12     92
+ Carmen Holmes       98833494     1.37         full-time    03/04/15     22
+ Meredith Lambert    29099997     2.97         full-time    03/08/11     23
+ Brendan Marsh       52894630     3.89         full-time    03/04/12     99
+ Kristen Mccarthy    87789460     2.57         part-time    12/04/13     37
+ Dora Blake          55898602     1.45         part-time    02/04/11     99
+ Raymond Lamb        89619733     2.68         part-time    03/04/15     22
+ Suzanne Briggs      18528076     3.4          full-time    03/08/13     97
+ Dana Miles          52972391     0.38         full-time    03/04/15     122
+ Tim Nichols         77370250     2.45         part-time    03/04/12     87
+ Edward Massey       54688476     3.66         part-time    03/01/15     32
+ Wesley Valdez       84301406     0.34         part-time    07/04/12     89
+ Mary Lee            94500541     3.67         full-time    01/04/13     98
+ Isabel Zimmerman    55418766     1.23         full-time    03/04/15     72
+ Barny Bas           24253552     3.35         part-time    03/12/12     23
+ Lillian Porter      24622366     1.5          full-time    10/12/14     12
+ Bernadette Taylor   57086018     0.98         part-time    03/11/15     32
+ Akane ka            24325235     3.9          full-time    03/12/15     34
+ Yvette Morgan       69459190     4            part-time    01/03/12     24
+ Jimmie Cook         94234524     0.98         full-time    08/04/11     87
+ Denise Goodman      83615900     3.45         part-time    03/03/14     22
+ Justin Moore        48544988     3.78         full-time    03/04/16     23
+ Kirk Holt           32667798     4            part-time    03/04/15     8
+ Nancy Cox           70892940     2.96         full-time    03/04/15     100
 */
 #endif
 
